9efd65980e2e0277edb550cb5fb7f21e3928d14c
JustinFlames12 <Justin.Dea3@gmail.com>
2021-02-08 15:59:30 -0500

    s

--- terminateworkspace.bat
+++ terminateworkspace.bat
@@ -1,1 +1,4 @@
 aws workspaces stop-workspaces --stop-workspace-requests WorkspaceId=ws-wp6n1c25n >> batchOutput.txt 2>&1
+Rem The line above is used to stop environments, the line below is used to terminate environments (can't recover after terminated')
+Rem ***Uncomment line 4 when ready***
+Rem aws workspaces terminate-workspaces --terminate-workspace-requests ws-wp6n1c25n >> batchOutput.txt 2>&1
